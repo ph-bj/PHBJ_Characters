724f6b91c87b9b88575eebbfb4b2aaf3d27fbaac
ph-bj <215520416+ph-bj@users.noreply.github.com>
2026-05-28 07:29:04 +0000

    feat: add story arc for Xiao Shun'er in chapter 5

--- src/characterAppearances/char-44.ts
+++ src/characterAppearances/char-44.ts
@@ -3,5 +3,10 @@
 import type { SceneBullet } from './types';
 
 export const char_44Appearances: Record<number, SceneBullet[]> = {
-  "5": [],
+  "5": [
+    {
+      en: "Xiao Shun'er, a master of the shibo xian variety performance, is hired by Fu San to entertain a wealthy young master (Hua Gongzi), but upon taking the stage in costume, the young master's face instantly turns furious, and his attendants immediately drive Xiao Shun'er away.",
+      zh: "十不闲状元小顺儿被富三请去伺候一位阔公子（华公子），但他刚上妆走上来，阔公子便登时怒容满面，跟班们连忙将小顺儿轰了下去。"
+    }
+  ],
 };
