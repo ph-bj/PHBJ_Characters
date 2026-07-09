cc1ff9b050d33aa91e7a0f0ed97b207c09cdf7f1
ph-bj <ffmaer+phbj@gmail.com>
2026-07-09 13:33:21 +1000

    Walkthrough - Project Cleanup I have cleaned up the project root directory by removing 16 obsolete/temporary scripts and log files that were left over from previous phases.
    Changes Made
    Removed the following files from the project:
    
    check_alignments.js
    check_lacunae.ts
    compare_ch1.txt
    compare_ch11.txt
    do_check.ts
    extract_component.py
    extract_verses.cjs
    fix_final.py
    move_utils.py
    plan.md
    run_check.cjs
    run_check.js
    scratch.js
    split_clean.py
    split_it.py
    test_eval.cjs

--- run_check.cjs
+++
@@ -1,2 +0,0 @@
-import { chapterTranslations3 } from './src/chapterTranslations/chapterTranslations3.ts';
-console.log(typeof chapterTranslations3, Array.isArray(chapterTranslations3), Object.keys(chapterTranslations3));
