2f00d4cb3e6aeb1eebd74eebb2afd081350dfd65
ph-bj <ffmaer+phbj@gmail.com>
2026-07-09 12:12:07 +1000

    我已经移除了 5 到 10 章的点评赏析数据。
    现在：
    
    appreciationDataRegistry 中不再注册 5-10 章的赏析内容。
    chapter4to10.ts 中仅保留了第 4 章的点评赏析内容，移除了其余章节的数据。

--- src/appreciationData/index.ts
+++ src/appreciationData/index.ts
@@ -1,6 +1,6 @@
 import { ChapterAppreciationData } from "../appreciationTypes";
 import { chapter1Appreciation, chapter2Appreciation, chapter3Appreciation } from "./chapter1to10";
-import { chapter4Appreciation, chapter5Appreciation, chapter6Appreciation, chapter7Appreciation, chapter8Appreciation, chapter9Appreciation, chapter10Appreciation } from "./chapter4to10";
+import { chapter4Appreciation } from "./chapter4to10";
 
 // A registry mapping chapter ID to its appreciation data.
 // In the future, we can implement dynamic imports or lazy loading here if this file grows too large.
@@ -9,12 +9,6 @@ export const appreciationDataRegistry: Record<number, ChapterAppreciationData> =
   2: chapter2Appreciation,
   3: chapter3Appreciation,
   4: chapter4Appreciation,
-  5: chapter5Appreciation,
-  6: chapter6Appreciation,
-  7: chapter7Appreciation,
-  8: chapter8Appreciation,
-  9: chapter9Appreciation,
-  10: chapter10Appreciation,
 };
 
 /**
